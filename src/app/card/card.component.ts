c028dca79d821a8547ec5835926ab7cb967908dc
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-02-21 11:48:54 -0600

    Mod(card.comp.ts)The input parameters were modified Add(abou.comp.html)Added the cards with the new input parameters

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,31 +1,19 @@
-import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
 
-export interface CardItem {
-  image: string;
-  title: string;
-  description: string;
-}
+
 
 @Component({
   selector:'app-card',
   standalone: true,
+  imports:[CommonModule],
   templateUrl:'./card.component.html',
   styleUrl: './card.component.css'
 })
 
 export class CardComponent{
- cardItems: CardItem[] = [
-  {
-    image:'../..//public/assets/olympusOne.JPG',
-    title:'MISIÓN',
-    description: 'Brindar un espacio seguro y confortable en un entorno amigable donde todos tengan la libertad de ser ellos mismos desarrollando sus valores y habilidades a través del deporte.'
-  },
-  {
-    image:'../..//public/assets/olympusAweasome.JPG',
-    title: 'VISIÓN',
-    description:'Ser el programa líder del cheerleading y gimnasia artística a nivel nacional en México.'
-  },
- ]
-
-
+ @Input() title: string = ''
+ @Input() image: string = ''
+ @Input() description: string = ''
+ @Input() items: string[] = []
 }
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -22,4 +22,14 @@ export class AboutComponent{
   },2000)
  }
 
+ misionImage: string = '../../..//public/assets/olympusOne.JPG'
+ misionDescription: string = 'Brindar un espacio seguro y confortable en un entorno amigable donde todos tengan la libertad de ser ellos mismos desarrollando sus valores y habilidades a través del deporte.'
+
+ visionImage: string = '../../..//public/assets/olympusAweasome.JPG'
+ visionDescription: string = 'Ser el programa líder del cheerleading y gimnasia artística a nivel nacional en México.'
+
+ filosofiaImage: string = '../../..//public/assets/olympusCuna.JPG'
+ filosofiaDescription = 'Nuestra filosofía es tener un lugar seguro y amigable para que cada uno de nuestros alumnos se desarrolle logrando sus objetivos personales.'
+
+
 }

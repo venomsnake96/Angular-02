fc1200787815a191db79c0acd36a913ca1414718
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-02-21 22:35:05 -0600

    Add(breadcrumb.comp) the component has been added along with the logic of its operation and the styles

---
+++ src/app/components/breadcrumb/breadCrumb.component.ts
@@ -0,0 +1,23 @@
+import { BreadcrumbService } from './breadCrumb.service';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { Breadcrumb } from './breadCrumb.model';
+
+@Component({
+  selector:'app-breadcrumb',
+  standalone:true,
+  imports:[CommonModule, RouterModule],
+  templateUrl:'./breadCrumb.component.html',
+  styleUrl:'./breadCrumb.component.css'
+})
+
+export class BreadcrumbComponent {
+breadcrumbs: Breadcrumb[] = [];
+
+constructor(private breadcrumbService: BreadcrumbService){
+  this.breadcrumbService.breadcrumbs$.subscribe((breadcrumbs) =>{
+    this.breadcrumbs = breadcrumbs
+  });
+}
+}
---
+++ src/app/components/breadcrumb/breadCrumb.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { Breadcrumb } from './breadCrumb.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BreadcrumbService {
+  private breadcrumbsSubject = new BehaviorSubject<Breadcrumb[]>([]);
+  breadcrumbs$ = this.breadcrumbsSubject.asObservable(); // Expone el Observable
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const breadcrumbs = this.createBreadcrumbs(this.router.routerState.snapshot.root);
+        this.breadcrumbsSubject.next(breadcrumbs); // Emite los nuevos breadcrumbs
+      });
+  }
+
+  private createBreadcrumbs(route: ActivatedRouteSnapshot, url: string = '', breadcrumbs: Breadcrumb[] = []): Breadcrumb[] {
+    const children: ActivatedRouteSnapshot[] = route.children;
+
+    if (children.length === 0) {
+      return breadcrumbs;
+    }
+
+    for (const child of children) {
+      const routeURL: string = child.url.map((segment) => segment.path).join('/');
+      if (routeURL !== '') {
+        url += `/${routeURL}`;
+      }
+
+      const label = child.data['title'] || child.routeConfig?.title;
+      if (label) {
+        breadcrumbs.push({ label, url });
+      }
+
+      return this.createBreadcrumbs(child, url, breadcrumbs);
+    }
+
+    return breadcrumbs;
+  }
+}

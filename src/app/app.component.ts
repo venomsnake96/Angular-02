53be86ee6785991cfcaae61785af176187e8edcd
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-02-23 22:09:16 -0600

    Add(footer.component)added footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navBar/navbar.component';
 import { BreadcrumbComponent } from './components/breadcrumb/breadCrumb.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ NavbarComponent, BreadcrumbComponent, RouterOutlet],
+  imports: [ NavbarComponent, BreadcrumbComponent, FooterComponent , RouterOutlet],
   standalone: true,
   templateUrl:'./app.component.html',
   styleUrl: './app.component.css'

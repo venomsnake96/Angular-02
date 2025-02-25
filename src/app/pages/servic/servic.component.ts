ee62832c9220d6ef386dddce2402db49f01baa9b
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-02-25 00:25:25 -0600

    Mod(servic.component)the template, styles and structure of the component were modified.

--- src/app/pages/servic/servic.component.ts
+++ src/app/pages/servic/servic.component.ts
@@ -29,25 +29,25 @@ export class ServiceComponent{
 
   cardItems: Carditem[] = [
     {
-      image:'',
+      image:'../../..//public/assets/gimnasia.png',
       title:'Gimnasia',
       description:'Programa especializado en gimnasia aerobica y acrobatica, desde principiante hasta experto',
       route: '/service/gimnasia'
     },
     {
-      image:'',
+      image:'../../..//public/assets/olympusPorra.JPG',
       title: 'Porra',
       description:'De nuestoros principales programas, apto para todas las edades',
       route: '/service/porra'
     },
     {
-      image: '',
+      image: '../../..//public/assets/telas.png',
       title: 'Telas',
-      description: '',
+      description: 'Esta disciplina es adecuada para personas de diferentes edades y niveles de condición física',
       route: ''
     },
     {
-      image: '',
+      image: '../../..//public/assets/cinICU.jpg',
       title: 'Competencias',
       description:'Contamos con programas de competencias locales e internacionales',
       route: ''

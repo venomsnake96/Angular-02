aec21729c9bf7a319dbe5db6526d28454e0dd8d7
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-02-26 17:57:47 -0600

    Mod(porra.comp.ts)Added logics and arrangements for displaying on the templete

--- src/app/pages/disciplinas/porra/porra.component.ts
+++ src/app/pages/disciplinas/porra/porra.component.ts
@@ -9,5 +9,8 @@ import { Component } from "@angular/core";
   styleUrl: './porra.component.css'
 })
 export class PorraComponent {
-  
+  title = 'Porra'
+  description = 'Un deporte de alto rendimiento, la animacion integra difetentes tecnicas y disciplinas como la gimnasia, el baile y las acrobacias en una rutina. Requiere un alto nivel de disciplina, resistencia y concentracion para llevarse acabo con precision. Si bien en su mayoria los equipos son femeniles, tambien contamos con categorias mixtas en diferentes niveles, desde basicos hasta los niveles mas avanzados.'
+  contexto = 'En Olympus podras encontrar que la animacion es una de nuestras actividades principales. Te acompañaremos paso a paso hasta alcanzar tu maximo potencial'
+
 }

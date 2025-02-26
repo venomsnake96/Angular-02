874dab7ae1cc1f0548da481d88c2edb8e4c3fe70
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-02-25 20:17:11 -0600

    Add(entrenamiento.comp.ts/telas.comp.ts)the main structure of the components has been added

---
+++ src/app/pages/disciplinas/entrenamiento/entrenamiento.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-trainig',
+  standalone: true,
+  imports:[CommonModule],
+  templateUrl: './entrenamiento.component.html',
+  styleUrl: './entrenamiento.component.css'
+})
+export class TrainigComponent{
+  
+}
---
+++ src/app/pages/disciplinas/telas/telas.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-telas',
+  standalone: true,
+  imports:[CommonModule],
+  templateUrl: './telas.component.html',
+  styleUrl:'./telas.component.css'
+})
+export class TelasComponent{
+  
+}

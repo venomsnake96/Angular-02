ea61dffccc0a1f0bdb967484e3ef7074f32e5be6
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-03-01 16:17:53 -0600

    Mod(app.cofig.ts/main.ts)the configuration of the routes has been modified.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
+import { provideRouter, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { DiscMainComponent } from './pages/disciplinas/disciplinasMain/disciplinasMain';
+import { GymnasticsComponent } from './pages/disciplinas/gimnasia/gimnasia.component';
+import { PorraComponent } from './pages/disciplinas/porra/porra.component';
+import { TelasComponent } from './pages/disciplinas/telas/telas.component';
+import { TrainigComponent } from './pages/disciplinas/entrenamiento/entrenamiento.component';
+
+
+const routes: Routes = [
+  {path: '',title:'Inicio', component: HomeComponent},
+  {path: 'about', title:'Nosotros', component:AboutComponent},
+  {path: 'disciplinas', title:'Disciplinas', component: DiscMainComponent,
+    children: [
+      {path: '', component:DiscMainComponent},
+      {path:'gimnasia', title: 'Gimnasia', component:GymnasticsComponent},
+      {path: 'porra', title: 'Porra', component: PorraComponent},
+      {path: 'telas', title: 'Telas', component: TelasComponent},
+      {path: 'entrenamiento', title:'Competencias', component:TrainigComponent}
+    ]
+  },
+  {path: '**', redirectTo: ''}
+];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync() ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter, Routes } from '@angular/router';
-import { AboutComponent } from './app/pages/about/about.component';
-import { HomeComponent } from './app/pages/home/home.component';
-import { GymnasticsComponent } from './app/pages/disciplinas/gimnasia/gimnasia.component';
-import { PorraComponent } from './app/pages/disciplinas/porra/porra.component';
-import { DisciplinasComponent } from './app/pages/disciplinas/disciplinas.component';
-import { TelasComponent } from './app/pages/disciplinas/telas/telas.component';
-import { TrainigComponent } from './app/pages/disciplinas/entrenamiento/entrenamiento.component';
-import { DiscMainComponent } from './app/pages/disciplinas/disciplinasMain/disciplinasMain';
 
-const routes: Routes = [
-  {path: '',title:'Inicio', component: HomeComponent},
-  {path: 'about', title:'Nosotros', component:AboutComponent},
-  {path: 'disciplinas', title:'Disciplinas', component: DisciplinasComponent,
-    children: [
-      {path: '', component:DiscMainComponent},
-      {path:'gimnasia', title: 'Gimnasia', component:GymnasticsComponent},
-      {path: 'porra', title: 'Porra', component: PorraComponent},
-      {path: 'telas', title: 'Telas', component: TelasComponent},
-      {path: 'entrenamiento', title:'Competencias', component:TrainigComponent}
-    ]
-  },
-  {path: '**', redirectTo: ''}
-];
 
-bootstrapApplication(AppComponent, {providers:[provideRouter(routes)],})
+
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));

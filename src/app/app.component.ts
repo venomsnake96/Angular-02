f6c7c566098cc16404868e75b91e8cf2693573df
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-03-02 01:51:56 -0600

    Add(loading.component)component added as lazy loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, Event, NavigationStart, NavigationEnd } from '@angular/router';
 import { NavbarComponent } from './components/navBar/navbar.component';
 import { BreadcrumbComponent } from './components/breadcrumb/breadCrumb.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoadingComponent } from './shared/loading/loading.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ NavbarComponent, BreadcrumbComponent, FooterComponent , RouterOutlet],
+  imports: [ NavbarComponent, BreadcrumbComponent, FooterComponent , RouterOutlet, LoadingComponent],
   standalone: true,
   templateUrl:'./app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+ isLoading: boolean = false;
 
-
+ constructor(private router: Router){
+  this.router.events.subscribe((event: Event)=>{
+    if(event instanceof NavigationStart) {
+      this.isLoading = true;
+    }else if (event instanceof NavigationEnd){
+      this.isLoading = false;
+    }
+  });
+ }
 
 }

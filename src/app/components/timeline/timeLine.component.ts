4ff86c4c06a6eb0816bf8909d0f1df9a1b736e5f
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-03-05 03:31:26 -0600

    Mod(timeLine.component)the logic of the component has been modified to automate the advance of the central line, still under construction.

--- src/app/components/timeline/timeLine.component.ts
+++ src/app/components/timeline/timeLine.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, ElementRef, HostListener, OnInit } from "@angular/core";
+import { expand } from "rxjs";
 
 @Component( {
   selector: 'app-timeline',
@@ -9,23 +10,73 @@ import { Component } from "@angular/core";
   styleUrl: './timeLine.component.css'
 })
 
-export class TimelineComponent {
+export class TimelineComponent implements OnInit {
   timelineItem = [
     {
       title: 'Evento 1',
-      description: 'descripcion del evento'
+      description: 'descripcion del evento',
+      expanded: false
     },
     {
       title: 'Evento 2',
-      description: 'descripcion del evento'
+      description: 'descripcion del evento',
+      expanded: false
     },
     {
       title: 'Evento 3',
-      description: 'descripcion del evento'
+      description: 'descripcion del evento',
+      expanded: false
     },
     {
       title: 'Evento 4',
-      description: 'descripcionn del evento'
+      description: 'descripcionn del evento',
+      expanded: false
+    },
+    {
+      title: 'Evento 5',
+      description: 'descripcion del evento',
+      expanded: false
+    },
+    {
+      title: 'Evento 6',
+      description: 'descripcion del evento',
+      expanded: false
     }
-  ]
+  ];
+
+  constructor(private elementRef: ElementRef) {}
+
+  toggleItem(index: number): void {
+    this.timelineItem[index].expanded = !this.timelineItem[index].expanded
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(){
+    this.animateTimelineLine();
+  }
+
+  animateTimelineLine(): void {
+    const timelineLine = document.getElementById('timeline-line');
+    if(timelineLine) {
+    const timelineElement = this.elementRef.nativeElement.querySelector('.timeline')
+    const timelineRect = timelineElement.getBoundingClientRect();
+    const scrollPosition = window.scrollY || window.pageYOffset;
+    const timelineTop = timelineRect.top + scrollPosition;
+    const timelineBottom = timelineTop + timelineRect.heigth;
+    const scrollProgress = (scrollPosition - timelineTop) / timelineRect.heigth;
+
+    if (scrollPosition >= timelineTop && scrollPosition <= timelineBottom)
+    {
+      timelineLine.style.transform = `scaleY(${scrollProgress})`;
+    }else if(scrollPosition < timelineTop){
+      timelineLine.style.transform = 'scaleY(0)';
+    }else if (scrollPosition > timelineBottom){
+      timelineLine.style.transform = 'scaleY(1)';
+    }
+    }
+  }
+
+ ngOnInit(): void {
+  this.animateTimelineLine()
+ }
 }

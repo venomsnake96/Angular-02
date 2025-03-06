c8960336e80fe5f90baf03778c0b350d0af0db01
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-03-05 20:42:19 -0600

    Mod(timeLine.component)the logic of the component and its styles have been modified

--- src/app/components/timeline/timeLine.component.ts
+++ src/app/components/timeline/timeLine.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from "@angular/common";
-import { Component, ElementRef, HostListener, OnInit } from "@angular/core";
-import { expand } from "rxjs";
+import { Component } from "@angular/core";
+
 
 @Component( {
   selector: 'app-timeline',
@@ -10,73 +10,95 @@ import { expand } from "rxjs";
   styleUrl: './timeLine.component.css'
 })
 
-export class TimelineComponent implements OnInit {
+export class TimelineComponent  {
   timelineItem = [
     {
-      title: 'Evento 1',
-      description: 'descripcion del evento',
+      year: '2006',
+      title: 'Los Inicios',
+      description: 'Un grupo de amigos se reunía en el parque “La Cantera” y entrenaban porra encima de tapetes foamy.',
       expanded: false
     },
     {
-      title: 'Evento 2',
-      description: 'descripcion del evento',
+      year: '2008',
+      title: 'Primer Sucursal',
+      description: 'Se inaugura el primer gimnasio en la privada Cedro 12 de Junio, únicamente con 7 líneas de matt.',
       expanded: false
     },
     {
-      title: 'Evento 3',
-      description: 'descripcion del evento',
+      year: '2009',
+      title: 'Mejoras',
+      description: 'Se instalan las primeras 7 líneas de pódium.',
       expanded: false
     },
     {
-      title: 'Evento 4',
-      description: 'descripcionn del evento',
+      year: '2010',
+      title: 'Worlds',
+      description: 'Olympus asisté por primera vez a la competencia mundial Worlds con el equipo de porra OMEGA.',
       expanded: false
     },
     {
-      title: 'Evento 5',
-      description: 'descripcion del evento',
+      year: '2013',
+      title: 'Sucursal Americas',
+      description: 'Con motivos de ampliación se cambio de sucursal, expandiendo así mismo la capacidad del gimnasio.',
       expanded: false
     },
     {
-      title: 'Evento 6',
-      description: 'descripcion del evento',
+      year: '2015',
+      title: 'Sucursal Periferico-Egipto',
+      description: 'Se redujo la capacidad de sucursal Américas para abrir una segunda sucursal por el periférico de la juventud.',
+      expanded: false
+    },
+    {
+      year: '2016',
+      title: 'Campeones Mundiales',
+      description: 'Equipos escolares del gimnasio ganan el campeonato mundial de preparatorias.',
+      expanded: false
+    },
+    {
+      year: '2019',
+      title: 'Cierre de una Sucursal',
+      description: 'En enero del 2019 se tuvo que cerrar la sucursal de periférico-egipto.',
+      expanded: false
+    },
+    {
+      year: '2020',
+      title: 'Pandemia',
+      description: 'Debido a las circunstancias que atrajo la pandemia del COVID-19, nos vimos forzados a cerrar temporalmente la academia.',
+      expanded: false
+    },
+    {
+      year: '2021',
+      title: 'Regreso a la Normalidad',
+      description: 'Se reanudan actividades dentro del gimnasio para buscar una nueva instalación.',
+      expanded: false
+    },
+    {
+      year: '2022',
+      title: 'Mejoras',
+      description: 'Nos mudamos de instalación y se incorporo a nuestros programas la disciplina de gimnasia artística',
+      expanded: false
+    },
+    {
+      year: '2023',
+      title: 'Worls',
+      description: 'Se fue adquiriendo todo el equipamiento de gimnasia artística tanto femenil como varonil.',
+      expanded: false
+    },
+    {
+      year: '2025',
+      title: 'Actualmente',
+      description: 'Actualmente en olympus estamos documentando todo aquello pertinente a la organización en beneficio de sistematizar y estandarizar los procesos. Además de que se ha integrado la disciplina de telas. Seguimos equipando y preparando la instalación, así como también la continua búsqueda de capacitar profesionalmente a nuestros instructores.',
       expanded: false
     }
+
   ];
 
-  constructor(private elementRef: ElementRef) {}
+
+
 
   toggleItem(index: number): void {
     this.timelineItem[index].expanded = !this.timelineItem[index].expanded
   }
 
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll(){
-    this.animateTimelineLine();
-  }
 
-  animateTimelineLine(): void {
-    const timelineLine = document.getElementById('timeline-line');
-    if(timelineLine) {
-    const timelineElement = this.elementRef.nativeElement.querySelector('.timeline')
-    const timelineRect = timelineElement.getBoundingClientRect();
-    const scrollPosition = window.scrollY || window.pageYOffset;
-    const timelineTop = timelineRect.top + scrollPosition;
-    const timelineBottom = timelineTop + timelineRect.heigth;
-    const scrollProgress = (scrollPosition - timelineTop) / timelineRect.heigth;
-
-    if (scrollPosition >= timelineTop && scrollPosition <= timelineBottom)
-    {
-      timelineLine.style.transform = `scaleY(${scrollProgress})`;
-    }else if(scrollPosition < timelineTop){
-      timelineLine.style.transform = 'scaleY(0)';
-    }else if (scrollPosition > timelineBottom){
-      timelineLine.style.transform = 'scaleY(1)';
-    }
-    }
-  }
-
- ngOnInit(): void {
-  this.animateTimelineLine()
- }
 }

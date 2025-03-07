2923027eae03605286c6a230251aac3bf85ba619
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-03-07 01:16:42 -0600

    Mod(gimnasia.component)information has been added

--- src/app/pages/disciplinas/gimnasia/gimnasia.component.ts
+++ src/app/pages/disciplinas/gimnasia/gimnasia.component.ts
@@ -10,6 +10,6 @@ import { Component } from "@angular/core";
 })
 export class GymnasticsComponent {
   title = 'Gimnasia'
-  description = ''
-  contexto = ''
+  description = 'Actividad física que busca mantener una buena forma física a través de ejercicios que implican fuerza, flexibilidad, agilidad y control corporal. Se practica tanto como deporte competitivo como actividad recreativa y se divide en varias disciplinas, como la gimnasia artística, rítmica y acrobática.'
+  contexto = 'En Olympus nos especializamos en la gimnacia ritmica y acrobatica. Adems de la practica individualizada de las disciplinas mencionadas, el programa de entrenamiento tiene como finalidad la integración a nuestro programa de animación'
 }

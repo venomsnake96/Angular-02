32e7dd0b44a398c9f74b0e14ed00f6f7d5aae38d
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-03-08 00:25:16 -0600

    Add(carInfo.component)the component was added to the page and template

--- src/app/pages/disciplinas/gimnasia/gimnasia.component.ts
+++ src/app/pages/disciplinas/gimnasia/gimnasia.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { InfoCardComponent } from "../../../components/cardInfo/cardInfo.component";
 
 @Component({
   selector:'app-gymnastics',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, InfoCardComponent],
   templateUrl: './gimnasia.component.html',
   styleUrl: './gimnasia.component.css'
 })
@@ -12,4 +13,19 @@ export class GymnasticsComponent {
   title = 'Gimnasia'
   description = 'Actividad física que busca mantener una buena forma física a través de ejercicios que implican fuerza, flexibilidad, agilidad y control corporal. Se practica tanto como deporte competitivo como actividad recreativa y se divide en varias disciplinas, como la gimnasia artística, rítmica y acrobática.'
   contexto = 'En Olympus nos especializamos en la gimnacia ritmica y acrobatica. Adems de la practica individualizada de las disciplinas mencionadas, el programa de entrenamiento tiene como finalidad la integración a nuestro programa de animación'
+
+  cards = [
+    {
+      title: 'Gimnasia Ritmica',
+      description: 'En esta modalidad, las gimnastas realizan rutinas con música en un piso de 13 x 13 metros, donde el aparato debe estar en constante movimiento y se incluyen elementos de gran amplitud con y sin giros, saltos, lanzamientos y captura de implementos. La gimnasia rítmica requiere un alto nivel de fuerza física, flexibilidad, coordinación y expresividad corporal.',
+      icon: '../../../..//public/assets/boombox.svg',
+      hoverImage: '../../../..//public/assets/gimnasiaBn.jpg'
+    },
+    {
+      title: 'Gimnasia Acrobatica',
+      description: 'La gimnasia acrobática es una disciplina gimnástica basada en rutinas que deben presentar giros, saltos y muestras de equilibrio y flexibilidad. Los ejercicios son realizados por equipos de entre dos y seis atletas, cuyos movimientos deben ser coordinados y armónicos.',
+      icon: '../../../..//public/assets/gimnasia.svg',
+      hoverImage: '../../../..//public/assets/gimnasia.png'
+    }
+  ]
 }

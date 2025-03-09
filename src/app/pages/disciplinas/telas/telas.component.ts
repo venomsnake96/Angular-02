38918d7dd9cf4b46bf99ba690539ad17824d01df
VenomSnake96 <claudiojaimez.sec3@gmail.com>
2025-03-09 01:25:05 -0600

    Mod(telas.component.ts)information has been added

--- src/app/pages/disciplinas/telas/telas.component.ts
+++ src/app/pages/disciplinas/telas/telas.component.ts
@@ -10,6 +10,8 @@ import { Component } from "@angular/core";
 })
 export class TelasComponent{
   title = 'Gimnasia Aerea';
-  description = '';
-  contexto = '';
+  description = 'Disciplina artística y acrobática que combina elementos de gimnasia, danza y circo. Consiste en realizar movimientos, figuras y acrobacias en el aire utilizando una o dos telas colgadas desde un punto fijo en altura. Es una actividad que requiere fuerza, flexibilidad, coordinación y expresión artística.';
+  contexto = 'Se emplean una o dos telas de tela elástica y resistente (generalmente de poliamida o nylon), que cuelgan de un soporte elevado, como un techo o una estructura especial. Las telas suelen tener entre 8 y 12 metros de largo, dependiendo de la altura del espacio.';
+
+  
 }
